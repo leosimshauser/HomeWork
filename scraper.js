0d6f7c51076d82cb656bd00bf2d66d807d50efd9
leosimshauser <lbrown26@stmichaels.vic.edu.au>
2026-06-03 10:11:07 +1000

    I have a working extension now!

--- scraper.js
+++ scraper.js
@@ -1,29 +1,29 @@
-const cheerio = require('cheerio');
+// import { load } from 'cheerio';
 
 
 
-async function Scrape() {
-    const url = 'https://learning.stmichaels.vic.edu.au/timetable';
-    const response = await fetch(url);
-    const html = await response.text();
-    console.log(html)
+// async function Scrape() {
+//     const url = 'https://learning.stmichaels.vic.edu.au/timetable';
+//     const response = await fetch(url);
+//     const html = await response.text();
+//     console.log(html)
 
 
-    const $ = cheerio.load(html);
+//     const $ = load(html);
  
-    // const text = $('.timetable-subject').text().trim();
+//     // const text = $('.timetable-subject').text().trim();
 
-    $('.timetable-subject a').each((i, el) => {
-    console.log($(el).text().trim());
-});
+//     $('.timetable-subject a').each((i, el) => {
+//     console.log($(el).text().trim());
+// });
 
-    // console.log(text);
+//     // console.log(text);
 
 
 
-};
-Scrape();
-// https://www.example.com
+// };
+// Scrape();
+// // https://www.example.com
 
-// https://learning.stmichaels.vic.edu.au/timetable
+// // https://learning.stmichaels.vic.edu.au/timetable
 
